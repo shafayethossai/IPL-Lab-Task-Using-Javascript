770eceb767498aa9b538972b8d8cd87a4252f6b7
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2024-10-06 01:00:51 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+// function sum(x, y) {
+//     return x+y;
+// }
+// function sub(x, y) {
+//     return x-y;
+// }
+// function mul(x, y) {
+//     return x*y;
+// }
+// function div(x, y) {
+//     return x/y;
+// }
+// let x = parseInt(prompt("take a user input: "));
+// let y = parseInt(prompt("take a another input: "));
+// let char = parseInt(prompt("which operation you want(+, -, *, /): "));
+// if (char = "+") {
+//     document.write("sum is : ", sum(x, y));
+// }
+// else if (char = "-") {
+//     document.write("sub is : ", sub(x, y));
+// }
+// else if (char = "*") {
+//     document.write("mul is : ", mul(x, y));
+// }
+// else {
+//     document.write("div is : ", div(x, y));
+// }
+
+// function convert(x) {
+//     let fahrenhit = (x * 9 / 5) + 32;
+//     return fahrenhit;
+// }
+// let celsius = parseInt(prompt("take s user input of celsius: "));
+
+// document.write("output is : ", convert(celsius));
+
+
+// let arr = [];
+// let max_val = -1;
+
+// for (let i=0; i<5; i++) {
+//     let x = parseInt(prompt(""));
+//     arr.push(x);
+// }
+
+// for (let i=0; i<5; i++) {
+//     if (arr[i] > max_val) {
+//         max_val = arr[i];
+//     }
+// }
+// console.log("output : ", max_val);
+
+function pass(arr) {
+    let even_arr = [];
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i]%2 == 0) {
+            even_arr.push(arr[i]);
+        }
+    }
+    return even_arr;
+}
+let arr = [];
+
+for (let i=0; i<5; i++) {
+    let x = parseInt(prompt(""));
+    arr.push(x);
+}
+let array = pass(arr);
+document.write(array);
