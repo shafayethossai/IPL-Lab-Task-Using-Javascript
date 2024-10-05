c86e1628cf2e278e525e3321fde0225e7c995c41
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2024-10-06 01:01:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
-// function sum(x, y) {
-//     return x+y;
-// }
-// function sub(x, y) {
-//     return x-y;
-// }
-// function mul(x, y) {
-//     return x*y;
-// }
-// function div(x, y) {
-//     return x/y;
-// }
-// let x = parseInt(prompt("take a user input: "));
-// let y = parseInt(prompt("take a another input: "));
-// let char = parseInt(prompt("which operation you want(+, -, *, /): "));
-// if (char = "+") {
-//     document.write("sum is : ", sum(x, y));
-// }
-// else if (char = "-") {
-//     document.write("sub is : ", sub(x, y));
-// }
-// else if (char = "*") {
-//     document.write("mul is : ", mul(x, y));
-// }
-// else {
-//     document.write("div is : ", div(x, y));
-// }
+// problem no - 01 ===============================================
 
-// function convert(x) {
-//     let fahrenhit = (x * 9 / 5) + 32;
-//     return fahrenhit;
-// }
-// let celsius = parseInt(prompt("take s user input of celsius: "));
+function sum(x, y) {
+    return x+y;
+}
+function sub(x, y) {
+    return x-y;
+}
+function mul(x, y) {
+    return x*y;
+}
+function div(x, y) {
+    return x/y;
+}
+let x = parseInt(prompt("take a user input: "));
+let y = parseInt(prompt("take a another input: "));
+let char = parseInt(prompt("which operation you want(+, -, *, /): "));
+if (char = "+") {
+    document.write("sum is : ", sum(x, y));
+}
+else if (char = "-") {
+    document.write("sub is : ", sub(x, y));
+}
+else if (char = "*") {
+    document.write("mul is : ", mul(x, y));
+}
+else {
+    document.write("div is : ", div(x, y));
+}
 
-// document.write("output is : ", convert(celsius));
+// problem no - 02 ================================================
+
+function convert(x) {
+    let fahrenhit = (x * 9 / 5) + 32;
+    return fahrenhit;
+}
+let celsius = parseInt(prompt("take s user input of celsius: "));
+
+document.write("output is : ", convert(celsius));
 
 
-// let arr = [];
-// let max_val = -1;
+// problem no - 03 ================================================
 
-// for (let i=0; i<5; i++) {
-//     let x = parseInt(prompt(""));
-//     arr.push(x);
-// }
+let arr = [];
+let max_val = -1;
 
-// for (let i=0; i<5; i++) {
-//     if (arr[i] > max_val) {
-//         max_val = arr[i];
-//     }
-// }
-// console.log("output : ", max_val);
+for (let i=0; i<5; i++) {
+    let x = parseInt(prompt(""));
+    arr.push(x);
+}
+
+for (let i=0; i<5; i++) {
+    if (arr[i] > max_val) {
+        max_val = arr[i];
+    }
+}
+console.log("output : ", max_val);
+
+// problem no - 04 ================================================
 
 function pass(arr) {
     let even_arr = [];
